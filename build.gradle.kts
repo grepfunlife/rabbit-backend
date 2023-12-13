c6c355eb3016480cd062c3afdb384bd124ab42eb
azat.erzin <azat.erzin@icloud.com>
2023-12-13 15:49:21 +0100

    RRB-env-vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.9.21"
@@ -30,9 +30,9 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 
     //db
     implementation("org.jetbrains.exposed:exposed-core:0.45.0")
@@ -43,10 +43,3 @@ dependencies {
     implementation("org.flywaydb:flyway-core:8.2.0")
 
 }
-
-//flyway {
-//    url = System.getenv("DB_URL")
-//    user = System.getenv("DB_USER")
-//    password = System.getenv("DB_PASSWORD")
-//    baselineOnMigrate = true
-//}
