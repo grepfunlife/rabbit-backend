735712606b3f0d20044028d72db91245a2e78ad0
Mia <smartundschon@gmail.com>
2023-12-17 11:01:24 +0100

    RRB-2

--- src/main/resources/db/migration/V1__create_profile_table.sql
+++ src/main/resources/db/migration/V1__create_profile_table.sql
@@ -1,4 +1,4 @@
-create table profile (
+create table profiles (
                          id serial primary key,
                          email varchar(100) unique,
                          password varchar(100),
